e7d792bb7f38efcf446688b84c28800c87e05f9b
Jamie Seward <jamie.d.seward@gmail.com>
2024-12-01 01:48:56 -0800

    improve code

--- src/day1.rs
+++ src/day1.rs
@@ -6,12 +6,12 @@ pub fn part1(input: &str) -> i32 {
     let mut l_values = Vec::<i32>::with_capacity(1000);
     let mut r_values = Vec::<i32>::with_capacity(1000);
     for line in input.lines() {
-        let parts : Vec<&str> = line.split_whitespace().collect();
-        l_values.push(parts[0].parse().unwrap());
-        r_values.push(parts[1].parse().unwrap());
+        let mut parts = line.split_whitespace();
+        l_values.push(parts.next().unwrap().parse().unwrap());
+        r_values.push(parts.next().unwrap().parse().unwrap());
     }
-    l_values.sort();
-    r_values.sort();
+    l_values.sort_unstable();
+    r_values.sort_unstable();
     let mut total_diff = 0;
     for i in 0..l_values.len() {
         total_diff += (l_values[i] - r_values[i]).abs();
@@ -27,19 +27,16 @@ pub fn part2(input: &str) -> i32 {
         let parts : Vec<&str> = line.split_whitespace().collect();
         l_values.push(parts[0].parse().unwrap());
         let r_v : i32 = parts[1].parse().unwrap();
-        match r_values.get_mut(&r_v) {
-            Some(v) => { *v += 1; }
-            None => { r_values.insert(r_v, 1); }
-        }
+        *r_values.entry(r_v).or_insert(0) += 1;
     }
     let mut score = 0;
-    l_values.iter().for_each(|v| {
-        match r_values.get(v) {
+    for v in l_values {
+        match r_values.get(&v) {
             None => {},
             Some(count) => {
                 score += v * count;
             }
         }
-    });
+    }
     score
 }
